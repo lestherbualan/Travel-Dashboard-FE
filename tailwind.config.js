fab6e7862f0376e05e6323959ba9f243e9b6a255
Lesther Bualan <lestherbualan1718@gmail.com>
2022-09-12 18:08:35 +0800

    Add card, modify tailwind config color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,20 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        'default': '#D1F366',
-        'secondary': '#141627',
-        'primary': '#D0D4E7' 
+        'primary': '#D1F366',
+        'secondary': '#EC502C',
+        'background': '#141627',
+        'card-background': '#1C1F37',
+        'heading-text': '#D0D4E7',
+        'body-text': '#626577'
       }
     },
   },
   plugins: [],
 }
+// #D1F366 (Booger Buster) - used as a Primary color
+// #EC502C (Flame) - used as a Secondary color
+// #141627 (Eerie Black) - Used as a background color
+// #1C1F37 (Yankees Blue) - used as a cards background
+// #D0D4E7 (Columbia Blue) - used for heading text
+// #626577 (Dark Electric Blue) - Used for body text
